704d02853cbddb17cbf2926058aa42acc7919946
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-01-03 00:52:43 -0500

    Complete exercise 2-4

--- course/2-variables/exercises/4-same_line_declarations/code.go
+++ course/2-variables/exercises/4-same_line_declarations/code.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	// declare here
+	averageOpenRate := 0.23
+	displayMessage := `is the average open rate of your messages`
+
+	// averageOpenRate, displayMessage := .23, "is the average open rate of your messages"
 
 	fmt.Println(averageOpenRate, displayMessage)
 }
